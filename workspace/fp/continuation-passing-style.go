99d39ef367f6e20e535e5027a3f4a48379a4db49
Maurício Witter <rwietter@zohomail.com>
2023-10-24 20:03:22 -0300

    add: csp with goroutines

--- workspace/fp/continuation-passing-style.go
+++ workspace/fp/continuation-passing-style.go
@@ -2,31 +2,10 @@ package fp
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func asyncAdd(a, b int, ch chan int) {
-	result := a + b
-	time.Sleep(2 * time.Second)
-	ch <- result
-}
-
-func ContinuationPassingStyle() {
-	ch := make(chan int)
-	defer close(ch)
-
-	go asyncAdd(3, 4, ch)
-
-	fmt.Print("Running...")
-
-	result := <-ch
-	fmt.Println(" -> Async Add:", result)
-}
-
-// -----------------------------------------------
-// ------------- WaitGroup Example ---------------
-// -----------------------------------------------
-/*
 func asyncAdd(a, b int, cont func(int)) {
 	go func() {
 		result := a + b
@@ -47,4 +26,3 @@ func ContinuationPassingStyle() {
 	fmt.Print("Running...")
 	wg.Wait()
 }
-*/
